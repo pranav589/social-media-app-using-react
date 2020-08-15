e794cb94cfabbd4e9b452cab683e1055a3933854
pranav589 <pranav.bakale@gmail.com>
2020-08-15 13:14:46 +0530

    3 files modified

--- components/SignInComp.js
+++ components/SignInComp.js
@@ -1,27 +1,68 @@
-import React from 'react'
-import {Input} from 'react-materialize'
+import React, { useState } from "react";
+import { TextInput, Button, Icon } from "react-materialize";
+import signIn from "../api/SignIn";
 
+export default props => {
+  const [data, setData] = useState({});
+  const [error, setError] = useState("");
 
-export default (props)=>{
-  return(
-    <div>
-      <div className='outerBox'>
-      <h4>Sign In</h4>
-        <div className="input-field">
-            <input id="email" type="email"/>
-            <label htmlFor="email">Email</label>
-          </div>
+  const onChangeText = (key, value) => {
+    const newData = { ...data };
+    newData[key] = value;
+    setData(newData);
+  };
 
-        <div className='input-field'>
-            <input type='password' id='password'/>
-            <label htmlFor='passowrd' type='password'>Password</label>
-        </div>
+  const onSubmit = async () => {
+    const result = signIn(data);
+    if (result === true) {
+      console.log("SIGN IN SUCCESSFUL");
+      setError("");
+    } else if (result === false) {
+      console.log("Sign Up FAILED");
+      setError("Email and Password Incorrect");
+    }
+  };
 
-        <p>Create a new account <span onClick={props.changeState('SU')}></span></p>
-
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center"
+      }}
+    >
+      <div className="outerBox w400">
+        <h4 style={{ marginBottom: 50 }}>Sign In</h4>
+        <TextInput
+          label="Email"
+          onChange={e => onChangeText("email", e.target.value)}
+        />
+        <TextInput
+          label="Password"
+          onChange={e => onChangeText("password", e.target.value)}
+          type="password"
+        />
+        <Button node="button" type="submit" waves="light" onClick={onSubmit}>
+          Submit
+          <Icon right>send</Icon>
+        </Button>
+        <p>
+        Create a new account{" "}
+          <span
+            onClick={() => {
+              props.changeState("SU");
+            }}
+            style={{
+              marginLeft: 10,
+              fontWeight: "600",
+              color: "green",
+              cursor: "pointer"
+            }}
+          >
+            Sign Up
+          </span>
+        </p>
+        {error && <div style={{ color: "red" }}>{error}</div>}
       </div>
     </div>
-  )
-}
-
-
+  );
+};
--- components/SignUpComp.js
+++ components/SignUpComp.js
@@ -1,61 +1,70 @@
-import React,{useState} from 'react'
-import {Input} from 'react-materialize'
-import signUp from '../api/SignUp'
-import SignUp from '../components/SignUpComp'
+import React, { useState } from "react";
+import { TextInput, Button, Icon } from "react-materialize";
+import signUp from "../api/SignUp";
 
-export default (props)=>{
+export default props => {
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: ""
+  });
 
-  const [data,setData]=useState({
-      firstName:'',
-      lastName:'',
-      email:'',
-      password:''
-    })
+  const onSubmit = async () => {
+    const result = signUp(data);
+    if (result === true) {
+      console.log("SIGN UP SUCCESSFUL");
+    } else if (result === false) {
+      console.log("Sign Up FAILED");
+    }
+  };
 
-  const submit=  ()=>{
-    const result= signUp(data)
-    if(result){
-      console.log('SIGN UP DONE')
-    }else if (result===false)(
-      console.log("NOT DONE")
-    )
-  }
+  const onChangeText = (key, value) => {
+    const newData = { ...data };
+    newData[key] = value;
+    setData(newData);
+  };
 
-  const onChangeText=(key,value)=>{
-    const newData={...data}
-    newData[key]=value;
-    setData(newData)
-  }
-
-  return(
-    <div style={{display:'flex', justifyContent:'center'}}>
-    
-      <div className='outerBox'>
-      <h4>Sign Up</h4>
-        <div className="input-field ">
-            <input id="firstName" type="text" onChange={(e)=>onChangeText('firstName',e.target.value)}/>
-            <label htmlFor="firstName">First Name</label>
-        </div>
-
-        <div className='input-field'>
-            <input id="lastName" type="text" onChange={(e)=>onChangeText('lastName',e.target.value)}/>
-            <label htmlFor="lastName">Last Name</label>
-        </div>
-        
-        <div className='input-field'>
-            <input id="email" type="email" onChange={(e)=>onChangeText('email',e.target.value)}/>
-            <label htmlFor="email">Email</label>
-        </div>    
-
-        <div className='input-field'>
-            <input id="password" type="password" onChange={(e)=>onChangeText('password',e.target.value)}/>
-            <label htmlFor="password">Password</label>
-          
-          </div>
-
-        <button onClick={submit}>Submit</button>  
-        <p>Already have an account? <span onClick={()=>{props.changeState('SI')}}>Sign In</span></p>
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center"
+      }}
+    >
+      <div className="outerBox w400">
+        <h4>Sign Up</h4>
+        <TextInput
+          label="First Name"
+          onChange={e => onChangeText("firstName", e.target.value)}
+        />
+        <TextInput
+          label="Last Name"
+          onChange={e => onChangeText("lastName", e.target.value)}
+        />
+        <TextInput
+          label="Email"
+          onChange={e => onChangeText("email", e.target.value)}
+        />
+        <TextInput
+          label="Password"
+          onChange={e => onChangeText("password", e.target.value)}
+        />
+        <Button node="button" type="submit" waves="light" onClick={onSubmit}>
+          Submit
+          <Icon right>send</Icon>
+        </Button>
+        <p>
+          Already have an account?
+          <span
+            onClick={() => {
+              props.changeState("SI");
+            }} style={{color:'green',fontWeight:600, cursor:'pointer'}}
+          >
+            Sign In
+          </span>
+        </p>
       </div>
     </div>
-  )
-}
+  );
+};
